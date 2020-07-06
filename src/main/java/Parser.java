b44e76acd2771f9fe0b5e973584ed2ad5614547e
o.omelyanenko <misteroov@yandex.ua>
2020-07-06 16:41:28 +0300

    уже ищет анекдоты, но схуйней br

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,24 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
 public class Parser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Document doc = Jsoup.connect("https://www.anekdot.ru/").get();
+
+        for (int i = 0; i < 5; i++) {
+
+
+            String body = doc.getElementsByAttributeValue("class", "text").get(i).html();
+            System.out.println(body);
+            System.out.println();
+        }
+    }
 
     }
-}
+
