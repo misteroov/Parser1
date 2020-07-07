25df8ff847c5e6c3a4d01033ed43fea2c0544937
o.omelyanenko <misteroov@yandex.ua>
2020-07-07 16:38:15 +0300

    отправляет анкдоты Не хочет нормально реагировать на слова

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,4 +1,5 @@
 
+import org.glassfish.jersey.server.Uri;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * Класс-обработчик поступающих к боту сообщений.
@@ -18,6 +20,7 @@ public class EchoBot extends TelegramLongPollingBot {
 
     {
         try {
+
             doc = Jsoup.connect("https://www.anekdot.ru").get();
         } catch (IOException e) {
             e.printStackTrace();
@@ -47,14 +50,19 @@ public class EchoBot extends TelegramLongPollingBot {
                 //Извлекаем объект входящего сообщения
                 Message inMessage = update.getMessage();
                 //Создаем исходящее сообщение
+
                 SendMessage outMessage = new SendMessage();
                 //Указываем в какой чат будем отправлять сообщение
                 //(в тот же чат, откуда пришло входящее сообщение)
                 outMessage.setChatId(inMessage.getChatId());
                 //Указываем текст сообщения
-                outMessage.setText(text.text());
-                //Отправляем сообщение
-                execute(outMessage);
+                if (inMessage.equals("Анекдот")) {
+                    outMessage.setText(text.text());
+                }
+                else {outMessage.setText(inMessage.getText());}
+                    //Отправляем сообщение
+                    execute(outMessage);
+
             }
         } catch (TelegramApiException e) {
             e.printStackTrace();
