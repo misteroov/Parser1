2c87d8cb43707ec43e3fafd8832d05ff3ee3ea54
o.omelyanenko <misteroov@yandex.ua>
2020-07-07 16:16:19 +0300

    Анекдоты уже закидывает 5 шт Добавил бот

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,29 +1,37 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.jsoup.select.Elements;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 
 
 import java.io.IOException;
 
 public class Parser {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
 
-       Document doc = Jsoup.connect("https://www.anekdot.ru").get();
-
-        Elements text = doc.getElementsByClass("text");
-      int i =0;
-        for (Element x:text) {
-            if(i<5){
-                System.out.println(x.text());
-                System.out.println();
-                i++;
-            }
-        }
-
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        botsApi.registerBot(new EchoBot());
     }
+}
 
-    }
+//       Document doc = Jsoup.connect("https://www.anekdot.ru").get();
+//
+//        Elements text = doc.getElementsByClass("text");
+//      int i =0;
+//        for (Element x:text) {
+//            if(i<5){
+//                System.out.println(x.text());
+//                System.out.println();
+//                i++;
+//            }
+//        }
+
+
+
 
