c6b68e5b3d372605709b898a16cc8c54d9a4869f
o.omelyanenko <misteroov@yandex.ua>
2020-09-15 10:13:48 +0300

    add test/java solution

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-        HashMap<String, Double> map = new HashMap<String, Double>();
+        HashMap<String, Double> map = new HashMap<>();
         String s;
 
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
@@ -14,21 +14,24 @@ public class Solution {
             String name = array[0];
             Double value = Double.valueOf(array[1]);
             Map.Entry<String,Double>            entry;
-
-            if (!map.isEmpty()) {it=map.entrySet().iterator();
+            it=map.entrySet().iterator();
+            if (!map.isEmpty()) {
                 while (it.hasNext()) {
                     entry = it.next();
                     if (name.equals(entry.getKey())){
                         double newValue = entry.getValue() + value;
-                        entry.setValue(value);
+                        entry.setValue(newValue);
+
 
                     }
                 }
+                if (!map.containsKey(name))
+                    map.put(name,value);
             }else map.put(name,value);
 
         }
         reader.close();
-        List<Double>list = new ArrayList<Double>(map.values());
+        List<Double>list = new ArrayList<>(map.values());
         Collections.sort(list);
         for (Map.Entry<String,Double>entry:map.entrySet()){
             if (entry.getValue().equals(list.get(list.size()-1))){
@@ -37,7 +40,6 @@ public class Solution {
         }
     }
 }
-
 
 
 
