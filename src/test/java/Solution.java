2b7f6325f868c5e6aea5e3070f388b0327ffb15e
o.omelyanenko <misteroov@yandex.ua>
2020-09-18 10:21:47 +0300

    add test/java solution

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -21,8 +21,9 @@ public class Solution {
         BufferedReader fr = new BufferedReader(new FileReader(fileName));
         String s;
         while ((s=fr.readLine())!=null){
-            if ((s.contains(words.get(0))&&s.contains(words.get(1)))||(s.contains(words.get(1))&&s.contains(words.get(2)))||
-                    (s.contains(words.get(0))&&s.contains(words.get(2))))
+            if ((s.matches(".*\\b"+words.get(0)+"\\b.*")&&s.matches(".*\\b"+words.get(1)+"\\b.*"))||(s.matches(".*\\b"+words.get(1)+"\\b.*")
+                    &&s.matches(".*\\b"+words.get(2)+"\\b.*"))||
+                    (s.matches(".*\\b"+words.get(0)+"\\b.*")&&s.matches(".*\\b"+words.get(2)+"\\b.*")))
                 System.out.println(s);
         }
         fr.close();
