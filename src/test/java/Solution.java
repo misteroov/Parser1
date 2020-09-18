91d8cc809488e7cd771c69b332482d0df4cfaf57
o.omelyanenko <misteroov@yandex.ua>
2020-09-18 09:55:13 +0300

    add test/java solution

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,37 +1,30 @@
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
-    public static TestString testString = new TestString();
+    public static List<String>words = new ArrayList<>();
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
 
-    public static void main(String[] args) {
-
-
-        PrintStream consoleStream = System.out;
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream stream = new PrintStream(outputStream);
-        System.setOut(stream);
-        testString.printSomething();
-        String result = outputStream.toString();
-        String[] resArr = result.split("\\r\\n");
-
-        System.setOut(consoleStream);
-
-        for (int i = 0; i < resArr.length; i++) {
-            System.out.println(resArr[i]);
-            if (i % 2 != 0)
-                System.out.println("JavaRush - курсы Java онлайн");
-        }
     }
-
-
-    public static class TestString {
-        public void printSomething() {
-            System.out.println("first");
-            System.out.println("second");
-            System.out.println("third");
-            System.out.println("fourth");
-            System.out.println("fifth");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        BufferedReader fr = new BufferedReader(new FileReader(fileName));
+        String s;
+        while ((s=fr.readLine())!=null){
+            if ((s.contains(words.get(0))&&s.contains(words.get(1)))||(s.contains(words.get(1))&&s.contains(words.get(2)))||
+                    (s.contains(words.get(0))&&s.contains(words.get(2))))
+                System.out.println(s);
         }
+        fr.close();
     }
 }
