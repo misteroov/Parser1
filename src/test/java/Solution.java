40c1175c2af43aa0f91676b9b30f58e31acba357
o.omelyanenko <misteroov@yandex.ua>
2020-10-06 15:05:16 +0300

    add test/java solution

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -4,25 +4,33 @@ import java.util.Map;
 import java.util.Properties;
 
 public class Solution {
-    public static Map<String,String> runtimeStorage = new HashMap<>();
-    public static Properties p=new Properties();
-    public static void save(OutputStream outputStream) throws Exception{
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+
+    public static void save(OutputStream outputStream) throws Exception {
+        Properties p = new Properties();
         p.putAll(runtimeStorage);
-        p.store(outputStream,null);
+        p.store(outputStream, null);
     }
-    public static void load (InputStream inputStream) throws Exception{
+
+    public static void load(InputStream inputStream) throws Exception {
+        Properties p = new Properties();
         p.load(inputStream);
-        runtimeStorage=new HashMap(p);
+        runtimeStorage = new HashMap(p);
     }
 
     public static void main(String[] args) {
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            FileInputStream fos =new FileInputStream(reader.readLine())){
-            load(fos);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        System.out.println(runtimeStorage);
 
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+
+
+                load(fos);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            System.out.println(runtimeStorage);
+
+        }
     }
 }
